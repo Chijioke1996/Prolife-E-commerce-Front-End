012a6a5582a2b6cea1d7f33527425d86b477ad9b
Chijioke1996 <111643913+Chijioke1996@users.noreply.github.com>
2023-03-28 14:17:39 +0100

    eigth6 commit

--- app.js
+++ app.js
@@ -10,3 +10,47 @@ container.addEventListener("mouseleave", () => {
     texts2.classList.remove("active")
     texts1.classList.remove("active")
 })
+
+const cardTemplate = document.querySelector("[data-template]")
+const userContainer = document.querySelector("[data-usercard-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", e => {
+    const Value = e.target.value.toLowerCase()
+
+
+
+    users.forEach(user => {
+        const isVisible =
+            user.name.toLowerCase().includes(Value) ||
+            user.email.toLowerCase().includes(Value)
+        user.element.classList.toggle("hide", !isVisible)
+
+
+    })
+})
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+        users = data.map(user => {
+            const card = cardTemplate.content.cloneNode(true).children[0]
+            const pic = card.querySelector("[data-pic]")
+            const description = card.querySelector("[data-description]")
+            pic.textContent = user.picture
+            description.textContent = user.description
+            userContainer.append(card)
+
+            return { pic: user.picture, description: user.description, element: card }
+
+
+
+
+
+
+
+        })
+    })
